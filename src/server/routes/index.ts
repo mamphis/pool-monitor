f92398ed125f0b59fdf9638185abea166107974a
Mattis Wacker <mattis.wacker@gmx.de>
2021-05-04 19:59:30 +0200

    Ability to shut down display

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -6,12 +6,16 @@ const router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     Display.it.setText('Hallo Welt')
-    return res.render('index');
+    return res.render('index',{message: "Set Default Text"});
 });
 
+router.get('/kill', (req: Request, res: Response, next: NextFunction) => {
+    Display.it.killDisplay();
+    return res.render('index', {message: "Display killed."});
+});
 
 router.post('/setText', (req: Request, res: Response, next: NextFunction) => {
-    return res.render('index');
+    return res.render('index',{message: "Set Text: "});
 });
 
 export { router as indexRouter };
