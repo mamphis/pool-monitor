6910ebde1cb12d4dc8643a3d8f4ca3a456248d99
Mattis Wacker <mattis.wacker@gmx.de>
2021-05-04 21:24:27 +0200

    Added Temperature Sensor

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,10 +1,12 @@
 import { Display } from "../lib/display";
+import { TemperatureSensorManager } from "../lib/temperature";
 import { Server } from "./server";
 
 export async function start() {
     const server = new Server(3000);
     Display.it;
-
+    TemperatureSensorManager.it;
+    
     await server.config();
     await server.start();
 }
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -6,16 +6,13 @@ const router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
     Display.it.setText('Hallo Welt')
-    return res.render('index',{message: "Set Default Text"});
-});
-
-router.get('/kill', (req: Request, res: Response, next: NextFunction) => {
-    Display.it.killDisplay();
-    return res.render('index', {message: "Display killed."});
+    return res.render('index', { message: "Set Default Text" });
 });
 
 router.post('/setText', (req: Request, res: Response, next: NextFunction) => {
-    return res.render('index',{message: "Set Text: "});
+    const { customText } = req.body;
+    Display.it.setText(customText);
+    return res.render('index', { message: `Set Text: "${customText}"` });
 });
 
 export { router as indexRouter };
