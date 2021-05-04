b101a542328cee9511373ab0ad448fbd55cd918e
Mattis Wacker <mattis.wacker@gmx.de>
2021-05-04 19:40:03 +0200

    Removed binding

--- src/lib/display.ts
+++ src/lib/display.ts
@@ -27,13 +27,13 @@ export class Display {
         });
 
         process.on('SIGINT', () => {
-            this.lcd.clear.bind(this.lcd)();
-            this.lcd.close.bind(this.lcd)();
+            this.lcd.clear();
+            this.lcd.close();
         });
 
         this.lcd.on('ready', () => {
             console.log(`Display is ready to show something.`);
-            this.lcd.clear.bind(this.lcd)()
+            this.lcd.clear()
             this.printable = true;
         });
     }
@@ -44,9 +44,9 @@ export class Display {
         }
 
         const lines = text.split('\n');
-        this.lcd.setCursor.bind(this.lcd)(0, 0);
-        this.lcd.print.bind(this.lcd)((lines[0] || '').substr(0, this.cols));
-        this.lcd.setCursor.bind(this.lcd)(0, 1);
-        this.lcd.print.bind(this.lcd)((lines[1] || '').substr(0, this.cols));
+        this.lcd.setCursor(0, 0);
+        this.lcd.print((lines[0] || '').substr(0, this.cols));
+        this.lcd.setCursor(0, 1);
+        this.lcd.print((lines[1] || '').substr(0, this.cols));
     }
 }
