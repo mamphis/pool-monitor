c4bb8394403850cb876a6a6fc348e8c94af96bda
Mattis Wacker <mattis.wacker@gmx.de>
2021-05-04 17:10:18 +0200

    Added express server and router

---
+++ src/server/server.ts
@@ -0,0 +1,35 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from 'cors';
+import moment from 'moment';
+import { indexRouter } from "./routes";
+
+export class Server {
+    private app: Application;
+
+    constructor(private port: number) {
+        this.app = express();
+    }
+
+    async config() {
+        this.app.use(express.json({ limit: '5mb' }));
+        this.app.use(cors());
+
+        this.app.use(async (req: Request, res: Response, next: NextFunction) => {
+            const start = moment();
+            await next();
+            const end = moment();
+
+            console.log(`(${end.diff(start)}ms) [${req.method}] ${req.originalUrl} => ${res.statusCode} ${res.statusMessage} ${req.method === 'POST' ? JSON.stringify(req.body) : ''}`);
+        });
+
+        this.app.use('/', indexRouter);
+    }
+
+    async start() {
+        this.app.listen(this.port, () => {
+            console.log(`Server started listening on port: ${this.port}`);
+        });
+    }
+}
+
+
