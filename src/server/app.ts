f69c1c67c2d96608191753ee79b608290f02d22c
Mattis Wacker <mattis.wacker@gmx.de>
2021-05-05 13:38:25 +0200

    added io management and context object

--- src/server/app.ts
+++ src/server/app.ts
@@ -6,7 +6,14 @@ export async function start() {
     const server = new Server(3000);
     Display.it;
     TemperatureSensorManager.it;
-    
+
     await server.config();
     await server.start();
+
+    process.on('SIGINT', () => {
+        Display.it.killDisplay();
+        process.exit(0);
+    });
+
+    
 }
