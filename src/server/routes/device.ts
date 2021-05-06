1588f8f0523cba705753b68dd824a3633acb6aa7
Mattis Wacker <mattis.wacker@gmx.de>
2021-05-06 22:35:40 +0200

    toggle devices

--- src/server/routes/device.ts
+++ src/server/routes/device.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response, Router } from "express";
 import moment from "moment";
 import { Context } from "../../lib/context";
 import { Display } from "../../lib/display";
+import { IO } from "../../lib/io";
 import { TemperatureSensorManager } from "../../lib/temperature";
 
 const router = Router();
@@ -12,6 +13,18 @@ router.get('/', (req: Request, res: Response, next: NextFunction) => {
 
 router.get('state', async (req: Request, res: Response, next: NextFunction) => {
     return res.json(Context.it.lastIOStates.temperatures);
+});
+
+router.post('/toggle/:device', async (req: Request, res: Response, next: NextFunction) => {
+    const { filter, pump } = req.params;
+    if (filter) {
+        await IO.it.toggleFilterState();
+        return res.json({ state: Context.it.filterState ? 'An' : 'Aus' });
+    }
+    if (pump) {
+        await IO.it.togglePumpState();
+        return res.json({ state: Context.it.pumpState ? 'An' : 'Aus' });
+    }
 });
 
 export { router as temperatureRouter };
