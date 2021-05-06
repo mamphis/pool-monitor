42c0dc7881721352e78b7b8f727cba2c69505e49
Mattis Wacker <mattis.wacker@gmx.de>
2021-05-06 21:29:03 +0200

    changed edge when callback is generated.

--- src/lib/io.ts
+++ src/lib/io.ts
@@ -18,8 +18,8 @@ export class IO extends EventEmitter {
     private constructor() {
         super();
 
-        this.btnFilter = new Gpio(CONST.PIN_BUTTON_FILTER, 'in', 'rising', { debounceTimeout: 10 });
-        this.btnPump = new Gpio(CONST.PIN_BUTTON_PUMP, 'in', 'rising', { debounceTimeout: 10 });
+        this.btnFilter = new Gpio(CONST.PIN_BUTTON_FILTER, 'in', 'falling', { debounceTimeout: 10 });
+        this.btnPump = new Gpio(CONST.PIN_BUTTON_PUMP, 'in', 'falling', { debounceTimeout: 10 });
 
         this.rlsFilter = new Gpio(CONST.PIN_RELAIS_FILTER, 'out');
         this.rlsPump = new Gpio(CONST.PIN_RELAIS_PUMP, 'out');
