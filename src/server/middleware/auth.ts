53dc44264c7bb1a28140520ed3787018016ceb1b
Mattis Wacker <mattis.wacker@gmx.de>
2021-07-06 10:30:38 +0200

    added functions to check for authorization
    This patch adds functions to check if the request is authorized and to
    check if the username and password is a valid stored combination.

--- src/server/middleware/auth.ts
+++ src/server/middleware/auth.ts
@@ -1,12 +1,14 @@
+import { compare } from "bcrypt";
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import jwt from 'jsonwebtoken';
+import { Context } from "../../lib/system/context";
 import { userCount } from "../../lib/system/user";
 import { randomString } from "../../lib/utils";
 
 const secret = randomString(20);
 
-export type payload = { username: string };
+export type payload = { username: string, password: string };
 
 export const getToken = (payload: payload): string => {
     return jwt.sign({ user: payload }, secret);
@@ -28,4 +30,40 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
             next(createHttpError(401));
         }
     }
+}
+
+export const isAuthed = (req: Request): boolean => {
+    try {
+        jwt.verify(req.signedCookies.user, secret) as { user: payload };
+        return true
+    } catch {
+        return false;
+    }
+}
+export async function login(username: string, password: string): Promise<boolean>;
+export async function login(cookie: string): Promise<boolean>;
+export async function login(usernameOrCookie: string, password?: string): Promise<boolean> {
+    let username, pwd: string;
+    if (!password) {
+        try {
+            const { user } = jwt.verify(usernameOrCookie, secret) as { user: payload };
+            username = user.username;
+            pwd = user.password;
+        } catch {
+            return false;
+        }
+    } else {
+        username = usernameOrCookie;
+        pwd = password;
+    }
+
+    const users = Context.it.users;
+
+    if (users[username]) {
+        if (await compare(pwd, users[username])) {
+            return true;
+        }
+    }
+
+    return false;
 }
