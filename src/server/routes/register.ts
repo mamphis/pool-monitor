b35527fadf50ae3e63113695a62668c189018fbd
mamphis <10652205+mamphis@users.noreply.github.com>
2024-04-20 13:50:30 +0200

    Do not display the register page if a user is already registered
    This patch checks if there is a user registered.
    It prevents anybody to register another user to gain access.

--- src/server/routes/register.ts
+++ src/server/routes/register.ts
@@ -2,10 +2,15 @@ import { hash } from "bcrypt";
 import { NextFunction, Request, Response, Router } from "express";
 import { Context } from "../../lib/system/context";
 import { userCount } from "../../lib/system/user";
+import { isAuthed } from "../middleware/auth";
 
 const router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
+    if (userCount() > 0 || isAuthed(req)) {
+        return res.redirect('/');
+    }
+    
     return res.render('register', {});
 });
 
