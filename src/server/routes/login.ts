5a0f89a74ce2325f32485e828865aaa123cb37a4
mamphis <10652205+mamphis@users.noreply.github.com>
2024-04-20 13:50:30 +0200

    Check if user is authorized
    This patch checks if the user is authorized and then redirects to the
    home page.
    
    When he tries to login, he will be redirected to the page he comes from.

--- src/server/routes/login.ts
+++ src/server/routes/login.ts
@@ -2,11 +2,15 @@ import { compare } from "bcrypt";
 import { NextFunction, Request, Response, Router } from "express";
 import createHttpError from "http-errors";
 import { Context } from "../../lib/system/context";
-import { getToken } from "../middleware/auth";
+import { getToken, isAuthed } from "../middleware/auth";
 
 const router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
+    if (isAuthed(req)) {
+        return res.redirect('/');
+    }
+
     return res.render('login', {});
 });
 
@@ -16,7 +20,7 @@ router.post('/', async (req: Request, res: Response, next: NextFunction) => {
     if (users[req.body.username]) {
         if (await compare(req.body.password, users[req.body.username])) {
             res.cookie('user', getToken(req.body), { signed: true });
-            return res.redirect('/');
+            return res.redirect((req.query.returnTo as string | undefined) ?? '/');
         }
     }
 
