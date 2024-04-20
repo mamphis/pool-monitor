ba0c2beb79a9befefcc33f63d4a94e984355e860
mamphis <10652205+mamphis@users.noreply.github.com>
2024-04-20 13:48:00 +0200

    Added functionallity to update script
    added
    - pull latest version
    - install dependencies
    - restart applicatiom

--- src/lib/system/update.ts
+++ src/lib/system/update.ts
@@ -1,3 +1,4 @@
+import { exec, spawn } from 'child_process';
 import semver from 'semver';
 import simpleGit from "simple-git/promise";
 import { Context } from "./context";
@@ -14,3 +15,30 @@ export async function newerVersionAvailable(): Promise<boolean> {
     return semver.gt(tags.latest, Context.it.installedVersion, { includePrerelease: true });
 }
 
+export async function pullLatestVersion() {
+    const git = simpleGit()
+    await git.fetch();
+    await git.checkoutLatestTag('', '')
+}
+
+export async function installDependencies(): Promise<void> {
+    return new Promise<void>((res) => {
+        exec('npm i', (err, stdout, stderr) => {
+            res();
+        });
+    })
+}
+
+export async function restartApplication() {
+    setTimeout(function () {
+        // When NodeJS exits
+        process.once("exit", function () {
+            const child = spawn(process.argv.shift() ?? '', process.argv, {
+                cwd: process.cwd(),
+                detached: true,
+                stdio: "inherit"
+            });
+        });
+        process.exit();
+    }, 1000);
+}
