9a5c0c18b4fd6504c1b7b92574f381190f2f04fa
mamphis <10652205+mamphis@users.noreply.github.com>
2024-04-20 13:50:48 +0200

    Updated packages and pins to new platine

--- src/server/routes/register.ts
+++ src/server/routes/register.ts
@@ -7,17 +7,15 @@ import { isAuthed } from "../middleware/auth";
 const router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    if (userCount() > 0 || isAuthed(req)) {
+    if (isAuthed(req)) {
         return res.redirect('/');
     }
-    
+
     return res.render('register', {});
 });
 
 router.post('/', async (req: Request, res: Response, next: NextFunction) => {
-    if (userCount() == 0) {
-        Context.it.setUser(req.body.username, hash(req.body.password));
-    }
+    Context.it.setUser(req.body.username, hash(req.body.password));
 
     return res.redirect('/login');
 });
