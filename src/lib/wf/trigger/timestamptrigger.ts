c92ca62bcf0dcc8f92d5a069271b294c76062a8a
mamphis <10652205+mamphis@users.noreply.github.com>
2024-04-20 13:39:09 +0200

    Bind cancelling call of job to the job

--- src/lib/wf/trigger/timestamptrigger.ts
+++ src/lib/wf/trigger/timestamptrigger.ts
@@ -19,7 +19,7 @@ export class TimestampTrigger extends ITrigger {
         });
 
         return {
-            cancel: job.cancel,
+            cancel: job.cancel.bind(job),
             nextInvocation: () => (job && job.nextInvocation()) ? moment(job.nextInvocation().toISOString()) : undefined
         }
     }
