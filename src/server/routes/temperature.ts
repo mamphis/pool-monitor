81493e6ae5dba86ae81d370df30c216867601483
mamphis <10652205+mamphis@users.noreply.github.com>
2024-04-20 13:38:47 +0200

    temperature display changes

--- src/server/routes/temperature.ts
+++ src/server/routes/temperature.ts
@@ -11,12 +11,13 @@ router.get('/', (req: Request, res: Response, next: NextFunction) => {
 
 router.get('/:id', async (req: Request, res: Response, next: NextFunction) => {
     const sensor = TemperatureSensorManager.it.sensor[req.params.id];
-    let returnObj: { sensors: string[], currentSensor?: { id: string, temperature: number } } = { sensors: TemperatureSensorManager.it.sensors, currentSensor: undefined };
+    let returnObj: { sensors: string[], currentSensor?: { id: string, temperature: number, formatted: string } } = { sensors: TemperatureSensorManager.it.sensors, currentSensor: undefined };
     if (sensor) {
         const temp = await sensor.getTemperature();
-        returnObj.currentSensor = { id: req.params.id, temperature: temp }
+        returnObj.currentSensor = { id: req.params.id, temperature: temp, formatted: await sensor.getTemperatureFormatted(temp) };
     }
 
+    Display.it.setText(`${returnObj.currentSensor?.id}\n${returnObj.currentSensor?.temperature}`);
     return res.render('temperature', returnObj);
 });
 
