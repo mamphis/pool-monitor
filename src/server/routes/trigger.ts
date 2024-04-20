411f1d6231057df19b79115ff622569d354275e1
mamphis <10652205+mamphis@users.noreply.github.com>
2024-04-20 13:38:52 +0200

    Added Page for new Trigger

--- src/server/routes/trigger.ts
+++ src/server/routes/trigger.ts
@@ -3,7 +3,11 @@ import { NextFunction, Request, Response, Router } from "express";
 const router = Router();
 
 router.get('/', (req: Request, res: Response, next: NextFunction) => {
-    return res.render('trigger', { message: "Set Default Text" });
+    return res.render('trigger', { trigger: [] });
+});
+
+router.get('/new', (req: Request, res: Response, next: NextFunction) => {
+    return res.render('newtrigger', {  });
 });
 
 export {router as triggerRouter};
